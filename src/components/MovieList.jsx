7700709f49afcc7f236903e28cc698642db6564d
WillianAmorim <willian.amorim1@outlook.com>
2021-09-01 16:33:43 -0300

    lint resolvendo

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,13 @@
 import React from 'react';
 
 class MovieList extends React.Component {
-    render() {
-        const { movies } = this.props
-        return(
-            <>
-            </>
-        );
-    }
+  render() {
+    const { movies } = this.props
+    
+    return(
+        <>{movies}</>
+    );
+  }
 }
 
 export default MovieList;
